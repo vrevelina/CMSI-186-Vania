f518e68b670114948c7a16ae2a1437dcc2743264
vrevelina <35344130+vrevelina@users.noreply.github.com>
2018-02-22 18:49:54 -0800

    HighRoll.java

---
+++ CMSI-186_Projects/Homework03/HighRoll.java
@@ -0,0 +1,20 @@
+/**
+ *  File name     :  HighRoll.java
+ *  Purpose       :  Program to play the game which uses DiceSet.java
+ *  Author        :  Vania Revelina
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+public class HighRoll {
+
+   public CountTheDays( String args[] ) {
+      dice = String.parseInt( args[0] );
+      sides = String.parseInt( args[1] );
+   }
+
+
+
+
+
+
+
+}
