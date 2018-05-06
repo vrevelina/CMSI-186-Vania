67e0ed6849deda60c2ba67c74cfdf5db1d683ae3
B.J. Johnson <bjohnson05@gmail.com>
2018-05-05 20:39:59 -0700

    added homework07 evaluation

---
+++ CMSI-186_Projects/Homework07/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"DynamicChangeMaker"},{"p":"<Unnamed>","l":"DynamicChangeMakerTestHarness"},{"p":"<Unnamed>","l":"Tuple"},{"p":"<Unnamed>","l":"TupleTestHarness"}]
